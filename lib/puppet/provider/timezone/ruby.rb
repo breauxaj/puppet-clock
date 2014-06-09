560dab15b34d3174b03fac9cf7543e32ac076bc6
Adam Breaux <breauxaj@gmail.com>
2014-06-09 10:17:39 -0400

    very direct method

--- lib/puppet/provider/timezone/ruby.rb
+++ lib/puppet/provider/timezone/ruby.rb
@@ -1,4 +1,8 @@
+require 'fileutils'
+
+lt = '/etc/localtime'
 tz = '/etc/sysconfig/clock'
+zi = '/usr/share/zoneinfo'
 
 Puppet::Type.type(:timezone).provide(:ruby) do
   desc "Timezone control"
@@ -6,5 +10,13 @@ Puppet::Type.type(:timezone).provide(:ruby) do
   confine :osfamily => :redhat
   confine :exists => tz
 
-  
+  if resource[:utc]
+    File.open(tz, 'w') { |file| file.write("ZONE=resource[:zone]\nUTC=resource[:utc]\n") }
+  else
+    File.open(tz, 'w') { |file| file.write("ZONE=resource[:zone]\n") }
+  end
+
+  FileUtils.cd(zi) do
+    FileUtils.cp "resource[:zone]", lt
+  end
 end
