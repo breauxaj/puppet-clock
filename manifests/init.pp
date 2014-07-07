52fe3eb060415a74c9b2a0cacba21218c16c31f9
Adam Breaux <breauxaj@gmail.com>
2014-07-07 14:37:30 -0400

    add docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: clock
+#
+# This class installs tzdata package
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the tzdata package
+#
+# Actions:
+#   - Install tzdata
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'clock':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'clock':
+#      ensure => 'absent'
+#    }
+#
 class clock (
   $ensure = 'latest'
 ){
