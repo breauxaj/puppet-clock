5b86d6b070b610944d6c11e5b7199ee50b3fc4e2
Adam Breaux <breauxaj@gmail.com>
2015-09-27 14:59:51 -0400

    now supports rhel 7 method

--- manifests/tz.pp
+++ manifests/tz.pp
@@ -23,25 +23,42 @@
 define clock::tz (
   $timezone
 ) {
+  include ::clock
+
   $prefix = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => '/usr/share/zoneinfo',
   }
-  
-  if $::osfamily == 'RedHat' {
-    file { '/etc/sysconfig/clock':
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => "ZONE=\"${timezone}\"\n",
-    }
+
+  case $::osfamily {
+    'redhat': {
+      case $::lsbmajdistrelease {
+        '6': {
+          file { '/etc/sysconfig/clock':
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => "ZONE=\"${timezone}\"\n",
+          }
     
-    file { '/etc/localtime': 
-      ensure => present, 
-      owner  => 'root', 
-      group  => 'root', 
-      mode   => '0644', 
-      source => "${prefix}/${timezone}",
+          file { '/etc/localtime': 
+            ensure => present, 
+            owner  => 'root', 
+            group  => 'root', 
+            mode   => '0644', 
+            source => "${prefix}/${timezone}",
+          }
+        }
+        default: {
+          exec { 'timedatectl':
+            path    => '/bin:/usr/bin',
+            command => "timedatectl set-timezone ${timezone}",
+          }
+        }
+      }
+    }
+    default: {
+      notify { 'OS not supported by this module': }
     }
   }
 
